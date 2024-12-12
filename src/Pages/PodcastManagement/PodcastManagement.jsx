5bf4fe029dcfb4399db5a54c6c0bdbde9efec701
shukumar-542 <shukumar542@gmail.com>
2024-12-12 16:09:06 +0600

    Enhancement podcast management select date functionlity

--- src/Pages/PodcastManagement/PodcastManagement.jsx
+++ src/Pages/PodcastManagement/PodcastManagement.jsx
@@ -1,8 +1,7 @@
-import { Table } from 'antd'
-import React from 'react'
+import { Checkbox, Form, List, Modal, Table } from 'antd'
+import React, { useState } from 'react'
 import { CiSearch } from 'react-icons/ci'
 import { FaArrowLeft } from 'react-icons/fa'
-import { LuCalendarClock } from 'react-icons/lu'
 import { Link } from 'react-router-dom'
 import img from '../../assets/images/user1.png'
 import img2 from '../../assets/images/user2.png'
@@ -10,6 +9,20 @@ import img3 from '../../assets/images/user3.png'
 import img4 from '../../assets/images/user4.png'
 
 const PodcastManagement = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const [selectedUsers, setSelectedUsers] = useState([]);
+
+    const onChange = (user) => {
+        const newSelectedUsers = selectedUsers.includes(user)
+            ? selectedUsers.filter((u) => u !== user)
+            : [...selectedUsers, user];
+        setSelectedUsers(newSelectedUsers);
+    };
+    const users = [
+        { name: 'Ronald Richards', avatar: 'https://randomuser.me/api/portraits/women/79.jpg' },
+        { name: 'Bessie Cooper', avatar: 'https://randomuser.me/api/portraits/women/32.jpg' },
+        { name: 'Esther Howard', avatar: 'https://randomuser.me/api/portraits/men/44.jpg' },
+    ];
     const dataSource = [
         {
             key: "#12331",
@@ -21,7 +34,7 @@ const PodcastManagement = () => {
             perticipant2: 'Jhon Smith',
             perticipant3: "Wade Warren",
             perticipant4: "Danne Rusell",
-            date : "12/07/24 at 4PM",
+            date: "12/07/24 at 4PM",
         },
         {
             key: "#12333",
@@ -33,13 +46,13 @@ const PodcastManagement = () => {
             perticipant2: 'Jhon Smith',
             perticipant3: "Wade Warren",
             perticipant4: "Danne Rusell",
-            date : "12/07/24 at 4PM",
+            date: "12/07/24 at 4PM",
 
         },
         {
             key: "#12334",
             perticipant1: "Holiday Parties",
-            date : "12/07/24 at 4PM",
+            date: "12/07/24 at 4PM",
             perticipant1Img: img,
             perticipant2Img: img2,
             perticipant3Img: img3,
@@ -47,7 +60,7 @@ const PodcastManagement = () => {
             perticipant2: 'Jhon Smith',
             perticipant3: "Wade Warren",
             perticipant4: "Danne Rusell",
-            date : "12/07/24 at 4PM",
+            date: "12/07/24 at 4PM",
         },
 
     ];
@@ -108,17 +121,27 @@ const PodcastManagement = () => {
                 </div>
             )
         },
-
+        {
+            title: "Select for Date",
+            dataIndex: "selectDate",
+            key: "selectDate",
+            render: (_, record) => (
+                <div onClick={() => setOpenModal(true)} className='text-[#FFA175]  inline-block text-center p-1 rounded-md cursor-pointer'>
+                    <p>Choose</p>
+                </div>
+            )
+        },
         {
             title: "Recording",
             dataIndex: "recording",
             key: "recording",
             render: (_, record) => (
                 <div className='text-[#FFA175]  inline-block text-center p-1 rounded-md cursor-pointer'>
-                   <p>Download</p>
+                    <p>Download</p>
                 </div>
             )
         },
+
     ];
     return (
         <div className='bg-white p-4 rounded-md'>
@@ -143,9 +166,29 @@ const PodcastManagement = () => {
 
 
             <Table dataSource={dataSource} columns={columns} className="custom-pagination" pagination={{
-                
-            }} />
 
+            }} />
+            <Modal open={openModal} centered footer={false} onCancel={() => setOpenModal(false)}>
+                <p className='text-xl font-medium text-center mb-10'>Choose for Date</p>
+                <Form>
+                    <Form.Item>
+                        <List className='px-24 border-none  ' 
+                            dataSource={users}
+                            renderItem={(user) => (
+                                <List.Item >
+                                    <Checkbox
+                                        checked={selectedUsers.includes(user.name)}
+                                        onChange={() => onChange(user.name)}
+                                    />
+                                    <img src={img4} alt="" />
+                                    <span>Ronald Richards</span>
+                                </List.Item>
+                            )}
+                        />
+                    </Form.Item>
+                    <button className='bg-[#FFA175] flex w-full justify-center items-center text-white py-2 rounded-sm'>Choose</button>
+                </Form>
+            </Modal>
 
         </div>
     )
