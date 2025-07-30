bb14eee127e80862ad0ea06e82d326a113596aac
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-07-30 18:05:03 +0600

    Update Vite server host configuration to correct IP address

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server :{
-    host: "10.10.10.47",
+    host: "10.10.10.36",
     port: "3002",
   }
 })
